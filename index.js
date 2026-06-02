03380ab896b65f1318ac5485149bb25934fedf36
Khushi Purohit <167404814+Khushii1712@users.noreply.github.com>
2026-06-02 14:50:12 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-import express from "express";
-import dotenv from "dotenv";
-import databaseConnection from "./utils/database.js";
-import cookieParser from "cookie-parser";
-import userRoute from "./routes/userRoute.js";
-import cors from "cors";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Provider } from "react-redux";
+import store from './redux/store';
 
-databaseConnection();
-
-dotenv.config({
-    path:".env"
-})
-
-const app = express();
-//middlewares 
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
-app.use(cookieParser());
-const corsOptions = {
-    origin:'http://localhost:3000',
-    credentials:true
-}
-app.use(cors(corsOptions));
- 
-// api
-app.use("/api/v1/user", userRoute);
-
-app.listen(process.env.PORT,() => {
-    console.log(`Server listen at port ${process.env.PORT}`);
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
